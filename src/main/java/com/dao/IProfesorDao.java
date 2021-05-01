d866456c4b27f45675deda668095d906176bd00a
Gustavo Laptop <gustavolonda@gmail.com>
2021-05-01 00:23:14 -0500

    Add dao

--- src/main/java/com/dao/IProfesorDao.java
+++ src/main/java/com/dao/IProfesorDao.java
@@ -1,9 +1,16 @@
 package com.dao;
 
+import java.util.Optional;
+
 import com.example.PruebaSpringBoot.entity.Profesor;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IProfesorDao extends CrudRepository<Profesor,Long>{
-    
+    public Profesor findByEmail(String email);
+    public Profesor findByEmailAndPassword(String email, String password);
+    public Optional<Profesor>  findById(Long id);
+    @Query("Select * from profesores p where p.id=?1")
+    public Profesor findByIdSQL(Long id);
 }
